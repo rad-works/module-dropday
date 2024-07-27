df9b0acd1de8b94bfb70db81e0e935e9116a9081
Dmitro Rudenko <rudenko.dmitro.dev@gmail.com>
2024-07-27 15:06:49 +0300

    Replace configurable product data with the simple product data

--- Plugin/Dropday/OrderAutomation/Helper/Data/ModifyRequestParamData.php
+++ Plugin/Dropday/OrderAutomation/Helper/Data/ModifyRequestParamData.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace DmiRud\Dropday\Plugin\Dropday\OrderAutomation\Helper\Data;
 
 use Dropday\OrderAutomation\Helper\Data;
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Sales\Model\Order;
@@ -11,11 +12,21 @@ use Magento\Sales\Model\Order;
 /**
  * Customize API request params:
  * - (legacy customization) use cost instead of price
+ * - replace configurable product data with the simple product data
  */
 class ModifyRequestParamData
 {
-    public function __construct(private readonly ProductRepositoryInterface $productRepository)
+    /**
+     * @var ProductRepositoryInterface $productRepository
+     */
+    private ProductRepositoryInterface $productRepository;
+
+    /**
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(ProductRepositoryInterface $productRepository)
     {
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -28,16 +39,52 @@ class ModifyRequestParamData
      */
     public function afterGetOrderRequestData(Data $subject, array $result, Order $order): array
     {
-        foreach (($result['products'] ?? []) as $index => $item) {
-            try {
-                $product = $this->productRepository->get($item['reference']);
+        $productsData = &$result['products'];
+        foreach ($productsData as $index => $data) {
+            if ($product = $this->getProduct($data['reference'], $order->getStoreId())) {
                 //Replace price with the cost
-                $result['products'][$index]['price'] = (float)$product->getCost();
-            } catch (NoSuchEntityException $e) {
+                $productsData[$index]['price'] = (float)$product->getCost();
+            }
+        }
+
+        /**
+         * Replace configurable product data with the simple product data
+         */
+        foreach ($order->getAllItems() as $item) {
+            if (!$item->getParentItemId()) {
                 continue;
+            }
+
+            foreach ($productsData as $index => $data) {
+                if ($item->getSku() !== $data['reference']) {
+                    continue;
+                }
+
+                $productsData[$index]['external_id'] = $item->getProductId();
+                $productsData[$index]['reference'] = $item->getSku();
+                if ($name = $this->getProduct($data['reference'], $order->getStoreId())?->getName()) {
+                    $productsData[$index]['name'] = $name;
+                }
             }
         }
 
         return $result;
+    }
+
+    /**
+     * Get product
+     *
+     * @param string $sku
+     * @param int|string|null $storeId
+     * @return ProductInterface|null
+     */
+    private function getProduct(string $sku, int|string $storeId = null): ?ProductInterface
+    {
+        try {
+            return $this->productRepository->get($sku, storeId: $storeId);
+            //Replace price with the cost
+        } catch (NoSuchEntityException) {
+            return null;
+        }
     }
 }
